a09e7d967e9e937dff2f3e822faf7f65e6b58599
Loïc Faugeron <faugeron.loic@gmail.com>
2024-12-11 20:08:56 +0000

    Calibrated Coding Standards as part of the testsuite

--- bin/test.sh
+++ bin/test.sh
@@ -1,10 +1,43 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+SHORT_HELP="$0 [-h] [-f]'"
+
+while getopts ":hf" opt; do
+    case $opt in
+        h)
+            echo 'Usage:'
+            echo "  $SHORT_HELP"
+            echo ''
+            echo 'Options:'
+            echo '  -f           Fix coding style'
+            echo ''
+            exit 0
+            ;;
+        f)
+            echo ''
+            echo '// Fixing CS...'
+            echo ''
+
+            vendor/bin/php-cs-fixer fix --allow-risky=yes
+            exit 0
+            ;;
+        \?)
+            echo '' >&2
+            echo "    [KO] The \"-$OPTARG\" option does not exist" >&2
+            echo '' >&2
+            echo "    $SHORT_HELP" >&2
+            echo '' >&2
+            exit 1
+            ;;
+    esac
+done
+unset SHORT_HELP
 
 echo ''
 echo '// Running tests...'
 
 composer --quiet dump --optimize
 
-vendor/bin/phpspec --no-interaction run -fdot #&& \
+vendor/bin/phpspec --no-interaction run -fdot && \
 #    vendor/bin/phpunit && \
-#    PHP_CS_FIXER_IGNORE_ENV=1 vendor/bin/php-cs-fixer fix --allow-risky=yes --dry-run
+    vendor/bin/php-cs-fixer fix --allow-risky=yes --dry-run
